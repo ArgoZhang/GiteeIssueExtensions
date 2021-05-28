8a82064306d3932e36b9fff72bc835f679c8c612
Argo Zhang <Argo@163.com>
2021-05-28 21:07:44 +0800

    init: 初始化仓库

---
+++ content-script.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var _continue = false;
+    var mo = new MutationObserver(function (mutationsList, observer) {
+
+    });
+
+    mo.observe(document, {
+        childList: true,
+        subtree: true
+    });
+
+    console.log('Gitee Issue auto fill has been registered');
+})();
