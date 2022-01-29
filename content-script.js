42f28a85dad1b47e49bfd468d0f9cd4eae2c6327
Argo <argo@163.com>
2022-01-29 14:04:46 +0800

    feat: 更新脚本适配新版 gitee

--- content-script.js
+++ content-script.js
@@ -1,24 +1,24 @@
-﻿(function () {
-    window.onload = function () {
+﻿(function() {
+    window.onload = function() {
         console.log('Gitee Issue auto fill has been registered');
 
         // dowork
         assigneesWork(0);
     };
 
-    var dowork = function (index, callback) {
+    var dowork = function(index, callback) {
         var fields = document.querySelectorAll('.issue-field-list .issue-field');
         var right = fields[index].querySelector('.issue-field-action');
         right.click();
 
-        var handler = window.setTimeout(function () {
+        var handler = window.setTimeout(function() {
             window.clearTimeout(handler);
             callback();
         }, 300);
     }
 
-    var assigneesWork = function (index) {
-        dowork(index, function () {
+    var assigneesWork = function(index) {
+        dowork(index, function() {
             var assignee = document.getElementById('issue-user-554725').querySelector('.btn-set-assignee');
             assignee.click();
 
@@ -26,17 +26,17 @@
         });
     };
 
-    var labelsWork = function (index) {
-        dowork(index, function () {
-            var label = document.querySelector('.issue-field-list .issue-field div[title="bug"]');
+    var labelsWork = function(index) {
+        dowork(index, function() {
+            var label = document.querySelector('.issue-field-list .issue-field div[title="feature"]');
             label.click();
 
             projectsWork(++index);
         });
     };
 
-    var projectsWork = function (index) {
-        dowork(index, function () {
+    var projectsWork = function(index) {
+        dowork(index, function() {
             var label = document.querySelector('.issue-field-list .issue-field div[data-value="70539"]');
             label.click();
 
@@ -44,26 +44,29 @@
         });
     };
 
-    var milestonesWork = function (index) {
-        dowork(index, function () {
-            var label = document.querySelector('.issue-field-list .issue-field div[data-value="93536"]');
+    var milestonesWork = function(index) {
+        dowork(index, function() {
+            var label = document.querySelector('.issue-field-list .milestone [data-program]');
             label.click();
 
             branchesWork(++index);
         });
     };
 
-    var branchesWork = function (index) {
-        dowork(index, function () {
-            var label = document.querySelector('.issue-field-list .issue-field div[data-value="refs/heads/dev"]');
+    var branchesWork = function(index) {
+        dowork(index, function() {
+            var label = document.querySelector('.issue-field-list .issue-field div[data-value="refs/heads/main"]');
+            if (label === null) {
+                label = document.querySelector('.issue-field-list .issue-field div[data-value="refs/heads/master"]');
+            }
             label.click();
 
             planedWork(++index);
         });
     };
 
-    var planedWork = function (index) {
-        dowork(index, function () {
+    var planedWork = function(index) {
+        dowork(index, function() {
             var label = document.querySelector('.datetimepicker-days .today');
             label.click();
         });
