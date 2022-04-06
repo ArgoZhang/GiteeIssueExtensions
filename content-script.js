25c35e75a40abdbb4342baf8760aba72ebbb3e37
Argo-Lenovo <argo@163.com>
2022-04-07 02:04:12 +0800

    chore: 更新脚本修复打开优先级问题

--- content-script.js
+++ content-script.js
@@ -1,24 +1,24 @@
-﻿(function() {
-    window.onload = function() {
+﻿(function () {
+    window.onload = function () {
         console.log('Gitee Issue auto fill has been registered');
 
         // dowork
         assigneesWork(0);
     };
 
-    var dowork = function(index, callback) {
+    var dowork = function (index, callback) {
         var fields = document.querySelectorAll('.issue-field-list .issue-field');
         var right = fields[index].querySelector('.issue-field-action');
         right.click();
 
-        var handler = window.setTimeout(function() {
+        var handler = window.setTimeout(function () {
             window.clearTimeout(handler);
             callback();
         }, 300);
     }
 
-    var assigneesWork = function(index) {
-        dowork(index, function() {
+    var assigneesWork = function (index) {
+        dowork(index, function () {
             var assignee = document.getElementById('issue-user-554725').querySelector('.btn-set-assignee');
             assignee.click();
 
@@ -26,8 +26,8 @@
         });
     };
 
-    var labelsWork = function(index) {
-        dowork(index, function() {
+    var labelsWork = function (index) {
+        dowork(index, function () {
             var label = document.querySelector('.issue-field-list .issue-field div[title="feature"]');
             label.click();
 
@@ -35,8 +35,8 @@
         });
     };
 
-    var projectsWork = function(index) {
-        dowork(index, function() {
+    var projectsWork = function (index) {
+        dowork(index, function () {
             var label = document.querySelector('.issue-field-list .issue-field div[data-value="70539"]');
             label.click();
 
@@ -44,8 +44,8 @@
         });
     };
 
-    var milestonesWork = function(index) {
-        dowork(index, function() {
+    var milestonesWork = function (index) {
+        dowork(index, function () {
             var label = document.querySelector('.issue-field-list .milestone [data-program]');
             label.click();
 
@@ -53,20 +53,20 @@
         });
     };
 
-    var branchesWork = function(index) {
-        dowork(index, function() {
+    var branchesWork = function (index) {
+        dowork(index, function () {
             var label = document.querySelector('.issue-field-list .issue-field div[data-value="refs/heads/main"]');
             if (label === null) {
                 label = document.querySelector('.issue-field-list .issue-field div[data-value="refs/heads/master"]');
             }
             label.click();
 
-            planedWork(++index);
+            planedWork(index + 4);
         });
     };
 
-    var planedWork = function(index) {
-        dowork(index, function() {
+    var planedWork = function (index) {
+        dowork(index, function () {
             var label = document.querySelector('.datetimepicker-days .today');
             label.click();
         });
